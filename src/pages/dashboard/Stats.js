c342d459c95db2c3b9fdf129e069920ecbd21d41
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2023-09-11 09:13:39 -0600

    Update Stats.js

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,11 +1,11 @@
 //dthis file is related to the ashboard>stats page
 import { useEffect } from "react";
-import { StatsContainer, Loading, ChartsContainer } from "../../components";
+import { StatsContainer, ChartsContainer } from "../../components";
 import { useDispatch, useSelector } from "react-redux";
 import { showStats } from "../../features/allJobs/allJobsSlice";
 
 const Stats = () => {
-  const { isLoading, monthlyApplications } = useSelector(
+  const { monthlyApplications } = useSelector(
     (store) => store.allJobs
   ); //from initialState in allJobsSlice.js bring these two properties
 
